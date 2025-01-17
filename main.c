0f57d51ea33faed6935ac491fbeb5339bf44f0bc
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2025-01-17 12:44:29 +0100

    Added Dec>Hex function

--- main.c
+++ main.c
@@ -30,30 +30,7 @@ int power(int value, int exponent) {
 
 char val[] = "1000";
 
-void convertBinToDec(char value[]) {
-    int len = strlen(value);
-    int decimal = 0;
-    for (int i = 0; i < len; i++) {
-        if (value[i] == '1') {
-            decimal += 1 << (len - i - 1);
-        }
-    }
-    printf("Binary (%s) -> Decimal: %d\n", value, decimal);
-}
-
-void convertDecToBin(char value[]) {
-    char res[30] = "";
-    int quotient = atoi(value);
-    int remain = 0;
-
-    char remainderString[30];
-    while (quotient > 0) {
-        remain = quotient % 2;
-        quotient = quotient / 2;
-        sprintf(remainderString, "%d", remain);
-        strncat(res, remainderString, strlen(remainderString));
-    }
-
+void reverse(char res[]) {
     // Reverse char array
     int l = 0;
     int r = strlen(res) - 1;
@@ -69,34 +46,84 @@ void convertDecToBin(char value[]) {
         l++;
         r--;
     }
-    // Print the final binary
-    printf("Dec (%s) -> bin: %s\n", value, res);
 }
 
-int main(void) {
-    printf("What do you want to convert?\n");
-    scanf("%s", inputNumber);
-    for (int i = 0; i < strlen(inputNumber); i++) {
-        if (!isdigit(inputNumber[i])) {
-            printf("%s, is not a valid number\n", inputNumber);
-            return 0;
+void convertBinToDec(char value[]) {
+    int len = strlen(value);
+    int decimal = 0;
+    for (int i = 0; i < len; i++) {
+        if (value[i] == '1') {
+            decimal += 1 << (len - i - 1);
+        }
+    }
+    printf("Binary (%s) -> Decimal: %d\n", value, decimal);
+}
+
+void convertDecToHex(char value[]) {
+    char res[30];
+    int quotient = atoi(value);
+    int remainder = 0;
+
+    while (quotient != 0) {
+        remainder = quotient % 16;
+        quotient = quotient / 16;
+
+        int rest = 0;
+        if (remainder > 9) {
+            rest = remainder - 10;
+            char c;
+            int i = 0;
+            // Converts the rest of the remainder into the corresponding Hex Character
+            for (c = 'A'; c <= 'F'; c++) {
+                if (i == rest) {
+                    // printf("%c", c);
+                    char remLetter[10] = "";
+                    sprintf(remLetter, "%c", c);
+
+                    strncat(res, remLetter, 1);
+                    break;
+                }
+                i++;
+            }
+        } else {
+            char rem[1];
+            sprintf(rem, "%d", remainder);
+            strncat(res, rem, 1);
         }
     }
 
-    ClearScreen();
-    convertDecToBin(inputNumber);
-    convertBinToDec(inputNumber);
-    // printf("Remainder %s\n", convertDecToBin("13"));
-    // printf("Result: %d", convertBinToDec(val));
-    // printf("Enter the value you'd like to convert\n"
-    //        "(Decimal '0d+yours' | Hexadecimal '0x'+yours | Binary '0b+yours')\n: ");
-    //
-    // for (int i = 0; i < sizeof(types) / sizeof(types[0]) ; i++) {
-    //     printf("%d) %s\n", i+1, types[i]);
-    // }
-    // printf("Which type you'd like to convert: ");
-    // scanf("%d", &convertInput);
-    //
-    // ClearScreen();
+    printf("Hexadecimal: %s\n", res);
+}
+
+void convertDecToBin(char value[]) {
+    char res[30] = "";
+    int quotient = atoi(value);
+    int remain = 0;
+
+    char remainderString[30];
+    while (quotient > 0) {
+        remain = quotient % 2;
+        quotient = quotient / 2;
+        sprintf(remainderString, "%d", remain);
+        strncat(res, remainderString, strlen(remainderString));
+    }
+
+    reverse(res);
+    // Print the final binary
+    printf("Dec (%s) -> Bin: %s\n", value, res);
+}
+
+int main(void) {
+    // printf("What do you want to convert?\n");
+    // // scanf("%s", inputNumber);
+    // // for (int i = 0; i < strlen(inputNumber); i++) {
+    // //     if (!isdigit(inputNumber[i])) {
+    // //         printf("%s, is not a valid number\n", inputNumber);
+    // //         return 0;
+    // //     }
+    // // }
+
+    convertDecToBin("359");
+    // convertDecToHex("35631");
     return 0;
 }
